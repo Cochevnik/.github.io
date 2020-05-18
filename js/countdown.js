5a434a2723e226eab9fb2c58098e149b7bb45edc
Cochevnik <65089299+Cochevnik@users.noreply.github.com>
2020-05-18 10:59:45 +0300

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -33,7 +33,7 @@ function CDT(){
  var tl = new Date('2020/05/18 11:05:00');
 
  // You can add time's up message here
-var timer = new CountdownTimer('CDT',tl,'<span class="number-wrapper"><div class="line"></div><span class="number end">У нас будет мальчик!</span></span>');
+ var timer = new CountdownTimer('CDT',tl,'<span class="number-wrapper"><div class="line"></div><span class="number end">У нас будет мальчик!</span></span>');
           timer.countDown();
 }
 window.onload=function(){
